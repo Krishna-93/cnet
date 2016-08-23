e75180a8d5aeb8f7da514e2e182e641354a1654b
varun <varunnprasaad@gmail.com>
2016-08-23 05:54:25 -0500

    Updated solution.html, services.html to use nav-pills instead of nav-tabs. slider on index.html is updated with new images.

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -24,44 +24,45 @@ jQuery(document).ready(function() {
 	    Slider 2
 	*/
 	$('.slider-2-container').backstretch([
-	  "assets/img/slider/5.jpg"
-	, "assets/img/slider/6.jpg"
-	, "assets/img/slider/7.jpg"
+	  "assets/img/slider/new/1.jpg",
+	  "assets/img/slider/new/2.jpg",
+	  "assets/img/slider/new/3.jpg",
+	  "assets/img/slider/new/4.jpg"
 	], {duration: 3000, fade: 750});
 	
 	/*
 	    Image popup (home latest work)
 	*/
-	$('.view-work').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: 'The image could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.parent('.work-bottom').siblings('img').attr('alt');
-			}
-		},
-		callbacks: {
-			elementParse: function(item) {
-				item.src = item.el.attr('href');
-			}
-		}
-	});
+	// $('.view-work').magnificPopup({
+	// 	type: 'image',
+	// 	gallery: {
+	// 		enabled: true,
+	// 		navigateByImgClick: true,
+	// 		preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	// 	},
+	// 	image: {
+	// 		tError: 'The image could not be loaded.',
+	// 		titleSrc: function(item) {
+	// 			return item.el.parent('.work-bottom').siblings('img').attr('alt');
+	// 		}
+	// 	},
+	// 	callbacks: {
+	// 		elementParse: function(item) {
+	// 			item.src = item.el.attr('href');
+	// 		}
+	// 	}
+	// });
 	
 	/*
 	    Flickr feed
 	*/
-	$('.flickr-feed').jflickrfeed({
-        limit: 8,
-        qstrings: {
-            id: '52617155@N08'
-        },
-        itemTemplate: '<a href="{{link}}" target="_blank" rel="nofollow"><img src="{{image_s}}" alt="{{title}}" /></a>'
-    });
+	// $('.flickr-feed').jflickrfeed({
+ //        limit: 8,
+ //        qstrings: {
+ //            id: '52617155@N08'
+ //        },
+ //        itemTemplate: '<a href="{{link}}" target="_blank" rel="nofollow"><img src="{{image_s}}" alt="{{title}}" /></a>'
+ //    });
 	
 	/*
 	    Google maps
@@ -157,63 +158,63 @@ jQuery(window).load(function() {
 	/*
 	    Portfolio
 	*/
-	$('.portfolio-masonry').masonry({
-		columnWidth: '.portfolio-box', 
-		itemSelector: '.portfolio-box',
-		transitionDuration: '0.5s'
-	});
+	// $('.portfolio-masonry').masonry({
+	// 	columnWidth: '.portfolio-box', 
+	// 	itemSelector: '.portfolio-box',
+	// 	transitionDuration: '0.5s'
+	// });
 	
-	$('.portfolio-filters a').on('click', function(e){
-		e.preventDefault();
-		if(!$(this).hasClass('active')) {
-	    	$('.portfolio-filters a').removeClass('active');
-	    	var clicked_filter = $(this).attr('class').replace('filter-', '');
-	    	$(this).addClass('active');
-	    	if(clicked_filter != 'all') {
-	    		$('.portfolio-box:not(.' + clicked_filter + ')').css('display', 'none');
-	    		$('.portfolio-box:not(.' + clicked_filter + ')').removeClass('portfolio-box');
-	    		$('.' + clicked_filter).addClass('portfolio-box');
-	    		$('.' + clicked_filter).css('display', 'block');
-	    		$('.portfolio-masonry').masonry();
-	    	}
-	    	else {
-	    		$('.portfolio-masonry > div').addClass('portfolio-box');
-	    		$('.portfolio-masonry > div').css('display', 'block');
-	    		$('.portfolio-masonry').masonry();
-	    	}
-		}
-	});
+	// $('.portfolio-filters a').on('click', function(e){
+	// 	e.preventDefault();
+	// 	if(!$(this).hasClass('active')) {
+	//     	$('.portfolio-filters a').removeClass('active');
+	//     	var clicked_filter = $(this).attr('class').replace('filter-', '');
+	//     	$(this).addClass('active');
+	//     	if(clicked_filter != 'all') {
+	//     		$('.portfolio-box:not(.' + clicked_filter + ')').css('display', 'none');
+	//     		$('.portfolio-box:not(.' + clicked_filter + ')').removeClass('portfolio-box');
+	//     		$('.' + clicked_filter).addClass('portfolio-box');
+	//     		$('.' + clicked_filter).css('display', 'block');
+	//     		$('.portfolio-masonry').masonry();
+	//     	}
+	//     	else {
+	//     		$('.portfolio-masonry > div').addClass('portfolio-box');
+	//     		$('.portfolio-masonry > div').css('display', 'block');
+	//     		$('.portfolio-masonry').masonry();
+	//     	}
+	// 	}
+	// });
 	
-	$(window).on('resize', function(){ $('.portfolio-masonry').masonry(); });
+	// $(window).on('resize', function(){ $('.portfolio-masonry').masonry(); });
 	
-	// image popup	
-	$('.portfolio-box h3').magnificPopup({
-		type: 'image',
-		gallery: {
-			enabled: true,
-			navigateByImgClick: true,
-			preload: [0,1] // Will preload 0 - before current, and 1 after the current image
-		},
-		image: {
-			tError: 'The image could not be loaded.',
-			titleSrc: function(item) {
-				return item.el.text();
-			}
-		},
-		callbacks: {
-			elementParse: function(item) {
-				var box_container = item.el.parents('.portfolio-box-container');
-				if(box_container.hasClass('portfolio-video')) {
-					item.type = 'iframe';
-					item.src = box_container.data('portfolio-big');
-				}
-				else {
-					item.type = 'image';
-					item.src = box_container.find('img').attr('src');
-				}
-			}
-		}
-	});
+	// // image popup	
+	// $('.portfolio-box h3').magnificPopup({
+	// 	type: 'image',
+	// 	gallery: {
+	// 		enabled: true,
+	// 		navigateByImgClick: true,
+	// 		preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+	// 	},
+	// 	image: {
+	// 		tError: 'The image could not be loaded.',
+	// 		titleSrc: function(item) {
+	// 			return item.el.text();
+	// 		}
+	// 	},
+	// 	callbacks: {
+	// 		elementParse: function(item) {
+	// 			var box_container = item.el.parents('.portfolio-box-container');
+	// 			if(box_container.hasClass('portfolio-video')) {
+	// 				item.type = 'iframe';
+	// 				item.src = box_container.data('portfolio-big');
+	// 			}
+	// 			else {
+	// 				item.type = 'image';
+	// 				item.src = box_container.find('img').attr('src');
+	// 			}
+	// 		}
+	// 	}
+	// });
 	
 	/*
 		Hidden images
